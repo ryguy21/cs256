3ae01bb1d17bd3d1ece4c7383c2b0af8df72ce8d
eaton11 <sameaton11@gmail.com>
2015-03-29 20:08:21 -0600

    Update index for html, css, and JavaScript
    Added animations to the input fields and submit button and made the
    page look more like the designed mock-up

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
 window.onload = function()
 {
-	var userIn = document.getElementById('username')
-	var passIn = document.getElementById('password')
-	var login = document.getElementById('login')
+	var userIn = document.getElementById('username');
+	var passIn = document.getElementById('password');
+	var login = document.getElementById('login');
 
 	userIn.onkeyup = passIn.onkeyup = function()
 	{
-		var username = userIn.value
-		var password = passIn.value
+		var username = userIn.value;
+		var password = passIn.value;
 
 		if (username != '' && password != '')
 		{
-			login.style.display = 'block'
+			login.style.visibility = "visible";
+			login.classList.add("visible");
 		}
 		else
 		{
-			login.style.display = 'none'
+			login.classList.remove("visible");	
+			login.style.visibility = "hidden";
 		}
 	}
 }
