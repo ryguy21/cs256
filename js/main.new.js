c7fcc77a54549c0b98f74580d786350fb90051ba
eaton11 <sameaton11@gmail.com>
2015-04-12 23:48:36 -0600

    fix issue where when click same sort by btn twice

--- js/main.new.js
+++ js/main.new.js
@@ -5,9 +5,6 @@ addLoadFunction(function()
 		var recent_act = document.getElementById("recent_act_btn");
 		var chapter = document.getElementById("chapter_btn");
 
-		chapter.className += " selected";
-	    recent_act.removeAttribute("selected");
-	    recent_act.className=recent_act.className.replace("selected","");
 
 	    var content = get('main-content')
 	    var headers = content.querySelectorAll('.chapter_header')
@@ -49,12 +46,6 @@ addLoadFunction(function()
 
 		var recent_act = document.getElementById("recent_act_btn");
 		var chapter = document.getElementById("chapter_btn");
-
-
-
-	    recent_act.className += " selected";
-	    chapter.removeAttribute("selected");
-	    chapter.className=chapter.className.replace("selected","");
 
 	    var content = get('main-content')
 	    var headers = content.querySelectorAll('.chapter_header')
@@ -103,50 +94,55 @@ addLoadFunction(function()
 	    }
 	}
 
+
+
 	function fadeOutVideos() {
+		// video container
 		var videosCont = document.getElementsByClassName("videos_cont");
-		
-		for(var i = 0, ii = videosCont.length; i < ii ; i++ )
-			videosCont[i].classList.add("fadeVideos");
-
+		for(var i = 0, ii = videosCont.length; i < ii ; i++ ) videosCont[i].classList.add("fadeVideos");
+		// chpater headers
 		var chapterHeaders = document.getElementsByClassName("chapter_header");
-		for(var i = 0, ii = chapterHeaders.length; i < ii ; i++ )
-			chapterHeaders[i].classList.add("fadeVideos");
+		for(var i = 0, ii = chapterHeaders.length; i < ii ; i++ ) chapterHeaders[i].classList.add("fadeVideos");
+		// selected btn
+		var sort_by_btn = document.getElementsByClassName("sort_by_btn");
+		for(var i = 0, ii = sort_by_btn.length; i < ii ; i++ ) sort_by_btn[i].classList.add("fadeVideos");
 	}
+
 	function fadeInVideos() {
+		// video container
 		var videosCont = document.getElementsByClassName("videos_cont");
-		
-		for(var i = 0, ii = videosCont.length; i < ii ; i++ )
-			videosCont[i].classList.remove("fadeVideos");
-
+		for(var i = 0, ii = videosCont.length; i < ii ; i++ ) videosCont[i].classList.remove("fadeVideos");
+		// chapter header
 		var chapterHeaders = document.getElementsByClassName("chapter_header");
-		for(var i = 0, ii = chapterHeaders.length; i < ii ; i++ )
-			chapterHeaders[i].classList.remove("fadeVideos");
+		for(var i = 0, ii = chapterHeaders.length; i < ii ; i++ ) chapterHeaders[i].classList.remove("fadeVideos");
+		// selected btn
+		var sort_by_btn = document.getElementsByClassName("sort_by_btn");
+		for(var i = 0, ii = sort_by_btn.length; i < ii ; i++ ) sort_by_btn[i].classList.remove("fadeVideos");
+	} 
 
+	function fadeController(evt){
+		var recent_act_btn = document.getElementById("recent_act_btn");
+		var chapter_btn = document.getElementById("chapter_btn");
+		//fade out content
+		fadeOutVideos();
+		setTimeout(function(){
+			if (evt.target == chapter_btn) { 
+				chapter_btn.classList.add("selected"); 
+				sortByChapter() 
+				recent_act_btn.classList.remove("selected");
+			} else{
+				recent_act_btn.classList.add("selected");
+				sortByActivity();
+				chapter_btn.classList.remove("selected"); 
+			}
+			fadeInVideos();
+		},300);
 	}
-
-
 
 	var el = document.getElementById("recent_act_btn");
-	el.addEventListener("click", function(){
-
-		fadeOutVideos();
-		setTimeout(function(){
-			sortByActivity();
-			fadeInVideos();
-		}, 250);
-			
-	});
-
+	el.addEventListener("click", fadeController);
 
 	var el = document.getElementById("chapter_btn");
-	el.addEventListener("click", function(){
+	el.addEventListener("click", fadeController);
 
-		fadeOutVideos();
-		setTimeout(function(){
-			sortByChapter();
-			fadeInVideos();
-		}, 250);
-			
-	});
 })
